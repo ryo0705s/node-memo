e9ab02587e36dffe217ec859013efabeb6800aea
ryo0705s <inspire.sasaki15@gmail.com>
2021-06-15 22:46:21 +0900

    index.jsを作成

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.render("hello.ejs");
+// });
 app.get("/", (req, res) => {
-  res.render("hello.ejs");
+  res.render("top.ejs");
+});
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.listen(3000);
